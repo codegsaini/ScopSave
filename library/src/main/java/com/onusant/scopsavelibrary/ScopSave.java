9c91a960cbe38f2cbd0f77727600b7318a71c558
Gaurav Saini <codegsaini@gmail.com>
2021-04-17 12:03:41 +0530

    Initial commit

---
+++ library/src/main/java/com/onusant/scopsavelibrary/ScopSave.java
@@ -0,0 +1,12 @@
+package com.onusant.scopsavelibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ScopSave {
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+}
